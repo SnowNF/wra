aa01975957957ead73a0ad42c452960b1b734a07
SnowNF <50768089+SnowNF@users.noreply.github.com>
2025-05-14 01:56:32 +0800

    fix input device refresh

--- chroot/refresh_inputs.sh
+++ chroot/refresh_inputs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+/usr/lib/systemd/systemd-udevd &
+
+sleep 1
+
 for device in /dev/input/event*; do
-    SYSTEMD_IGNORE_CHROOT=1 sudo -E udevadm test "$device"
+    SYSTEMD_IGNORE_CHROOT=1 sudo -E  udevadm trigger "$device"
     sudo chmod 777 "$device"
 done
 libinput list-devices
